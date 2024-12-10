079a5266894a2db3cf1ce98afa4b9165b6093c61
mukshith <mukshith333@gmail.com>
2024-12-10 12:55:14 +0530

    App.jsx - browser router removed, package.json - name changed to e-shops, index.html - base path removed

--- src/App.jsx
+++ src/App.jsx
@@ -34,19 +34,19 @@ const App = () => {
         />
         <Navbar />
         <SearchBar />
-        <BrowserRouter basename="/e-shops">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/collection" element={<Collection />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/products/:productId" element={<Product />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/place-order" element={<PlaceOrder />} />
-            <Route path="/orders" element={<Orders />} />
-          </Routes>
-        </BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/products/:productId" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/place-order" element={<PlaceOrder />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+
         <Footer />
       </div>
     </div>
